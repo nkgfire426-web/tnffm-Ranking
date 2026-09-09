b47c86f4a9f78143621445a062175652b0a1ace0
Niran <nkgfire426@gmail.com>
2026-09-09 20:22:09 +0800

    remove legacy team dashboard

--- create-a-professional-esports-community-leaderboard/src/app/team-dashboard/performance/page.tsx
+++
@@ -1,2 +0,0 @@
-import TeamDashboardSection from "@/components/TeamDashboardSection";
-export default function TeamDashboardPerformancePage(){ return <TeamDashboardSection section="performance"/>; }
